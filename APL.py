ea843ad3f67f55298dadff1d4c608e2e6aea6dbe
EvaldasSt <evaldas.stremas123@gmail.com>
2021-04-26 18:04:43 +0300

    - Rewrited glucose generating code. - Added glucose data functions.

--- APL.py
+++ APL.py
@@ -2,15 +2,67 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.axis([0, 10, 0, 1])
+# Inputs
+first_glucose_reading = 9
+cycles = 10
+daily_insulin_units = 66
 
-glucose_level = 6
 
-for i in range(100):
-    y = glucose_level
-    plt.scatter(i, y)
-    #glucose_level += random.uniform(0, 0.2)
-    glucose_level += (random.choice((-1, 1)) * random.uniform(0, 0.2))
-    plt.pause(0.05)
+def print_glucose_data(first_glucose_value, cycles):
+    x = range(cycles)
+    y = []
 
-plt.show()
+    for i in range(cycles):
+        y.append(first_glucose_value)
+        first_glucose_value += (random.choice((-1, 1)) * random.uniform(0, 0.2))
+
+    plt.plot(x, y)
+    plt.show()
+
+
+def get_glucose_data(first_glucose_value, cycles):
+    x = range(cycles)
+    y = []
+
+    for i in range(cycles):
+        y.append(first_glucose_value)
+        first_glucose_value += (random.choice((-1, 1)) * random.uniform(0, 0.2))
+    return y
+
+
+def get_insulin_sensitivity(daily_insulin_units):
+    insulin_sensitivity = 100 / daily_insulin_units
+    return insulin_sensitivity
+
+
+def get_glucose_data_delta(glucose_data, cycles):
+    x = range(cycles)
+    y = []
+
+    for i in range(cycles):
+        glucose_delta = glucose_data[i] - glucose_data[i - 1]
+        y.append(glucose_delta)
+    return y
+
+
+def print_glucose_data_delta(glucose_data, cycles):
+    x = range(cycles)
+    y = []
+
+    for i in range(cycles):
+        glucose_delta = glucose_data[i] - glucose_data[i - 1]
+        y.append(glucose_delta)
+    plt.plot(x, y)
+    plt.show()
+
+
+glucose_data = get_glucose_data(first_glucose_reading, cycles)
+print(glucose_data)
+
+glucose_data_delta = get_glucose_data_delta(glucose_data, cycles)
+print(glucose_data_delta)
+
+insulin_sensitivity = get_insulin_sensitivity(daily_insulin_units)
+print(insulin_sensitivity)
+
+print_glucose_data_delta(glucose_data, cycles)
