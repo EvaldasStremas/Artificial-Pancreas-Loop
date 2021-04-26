96854fced3c7d5f5309c2559ebe8356963a27e2e
EvaldasSt <evaldas.stremas123@gmail.com>
2021-04-26 15:18:19 +0300

    Added glucose generator

--- APL.py
+++ APL.py
@@ -1,1 +1,16 @@
-print(labas)
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.axis([0, 10, 0, 1])
+
+glucose_level = 6
+
+for i in range(100):
+    y = glucose_level
+    plt.scatter(i, y)
+    #glucose_level += random.uniform(0, 0.2)
+    glucose_level += (random.choice((-1, 1)) * random.uniform(0, 0.2))
+    plt.pause(0.05)
+
+plt.show()
